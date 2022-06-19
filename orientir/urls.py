cb8771b3bb133f5373ce2d1da1b818101090d219
acidmikk <sv.acid6@gmail.com>
2022-06-19 21:12:41 +0400

    restart

--- orientir/urls.py
+++ orientir/urls.py
@@ -25,4 +25,7 @@ urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('admin/', admin.site.urls),
     path('user-info/', orientir_core.user_info)
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
