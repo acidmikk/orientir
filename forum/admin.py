8b7975274eaca9f16e69db75796f775d8c9fb277
acidmikk <sv.acid6@gmail.com>
2022-06-21 15:16:48 +0400

    forum view work

--- forum/admin.py
+++ forum/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django import forms
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
 from django.utils.safestring import mark_safe
 from .models import *
@@ -14,4 +15,25 @@ class BoardAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
 
 
+class TopicAdminForm(forms.ModelForm):
+    subtitle = forms.CharField(label='Описание', widget=CKEditorUploadingWidget())
 
+    class Meta:
+        model = Topic
+        fields = '__all__'
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'last_update', 'board', 'starter')
+    list_display_links = ('subject', 'last_update')
+    search_fields = ('subject', 'last_update')
+    prepopulated_fields = {'slug': ('subject',)}
+    form = TopicAdminForm
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('message', 'topic', 'created_at', 'created_by')
+    list_display_links = ('message', 'topic')
+    search_fields = ('message', 'topic', 'created_at', 'created_by')
